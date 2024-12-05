b8c1d65212ddff91b9dc7a62900e840e02142d9a
Mirtilla-Kerplunk <melcherpradoalves@gmail.com>
2024-12-05 11:38:01 -0400

    Add files via upload

---
+++ EX01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01 PHP</title>
+</head>
+<body>
+<?php 
+echo "incrivel a semelhança que eu tenho com o ziggs de League of Legends, além de sermos literalmente iguais... temos o mesmo mindset, ambos somos extremamente frios e estrategistas em nossa forma de pensar e agir... bizarro '-' ele me representa muito, somos dois deuses nesse mundo."
+?>
+</body>
+</html>
